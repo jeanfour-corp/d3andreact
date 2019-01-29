66b87e06deca65449ca94a21a25340768fde6eec
jeanfour-corp <43960437+jeanfour-corp@users.noreply.github.com>
2019-01-29 08:17:29 +0100

    get rid of unneccessary configuration of AWS Amplify

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from "react";
 import './index.css';
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
 
 import App from "./App";
 const app = document.getElementById('app');
-
-Amplify.configure(aws_exports);
 
 ReactDOM.render(<App />,app);
 
